71ee3c7c76ee07d741103e7d8c38efe490df5cfd
Carlos Salguera <carlos.e.salguera@gmail.com>
2022-12-16 09:36:19 -0500

    stub up html, css, and js files & directories

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Cached Element References -----------------------------------------------
+
+// Constants ---------------------------------------------------------------
+
+// Variables ---------------------------------------------------------------
+
+let winner
+let tie 
+let turn
+let grid = [
+  null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null
+]
+
+// Event Listeners ---------------------------------------------------------
+
+// Functions ---------------------------------------------------------------
