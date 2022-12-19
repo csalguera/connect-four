32473b15ef3cecea04a7969043e12448629d8cd5
Carlos Salguera <carlos.e.salguera@gmail.com>
2022-12-19 10:43:19 -0500

    edit fromRowOne function to remove while loop as it is dead code

--- js/app.js
+++ js/app.js
@@ -207,20 +207,19 @@ function placePiece(idx) {
 }
 
 function fromRowOne(idx) {
-  while (!grid[idx]) {
-    if (!grid[idx + 35]) {
-      idx += 35
-    } else if (!grid[idx + 28]) {
-      idx += 28
-    } else if (!grid[idx + 21]) {
-      idx += 21
-    } else if (!grid[idx + 14]) {
-      idx += 14
-    } else if (!grid[idx + 7]) {
-      idx += 7
-    }
-    grid[idx] = turn
+  if (grid[idx]) return
+  if (!grid[idx + 35]) {
+    idx += 35
+  } else if (!grid[idx + 28]) {
+    idx += 28
+  } else if (!grid[idx + 21]) {
+    idx += 21
+  } else if (!grid[idx + 14]) {
+    idx += 14
+  } else if (!grid[idx + 7]) {
+    idx += 7
   }
+  grid[idx] = turn
 }
 
 function fromRowTwo(idx) {
